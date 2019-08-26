4d3bdad9317bf606c1d4abd1b5ce04fbb91dbaf3
Richard <2336400051@qq.com>
2019-08-26 16:43:52 +0800

    20190826

---
+++ src/main/java/jvm/day0820/Test01.java
@@ -0,0 +1,14 @@
+package jvm.day0820;
+
+/**
+ * @author wangchengdong
+ * @date 2019/8/20
+ * @desc
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("main 方法");
+        Test02.a = 10;
+    }
+}
---
+++ src/main/java/jvm/day0820/Test02.java
@@ -0,0 +1,13 @@
+package jvm.day0820;
+
+/**
+ * @author wangchengdong
+ * @date 2019/8/20
+ * @desc
+ */
+public class Test02 {
+    static{
+        System.out.println("Test02初始化...");
+    }
+    public static int a;
+}
